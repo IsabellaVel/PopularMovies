1e6eadf11c1385696c183050d587df1172c800a8
noahkim <noahkim03@gmail.com>
2016-11-06 16:07:42 -0800

    update custom adapter

--- app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -2,11 +2,14 @@ package com.example.android.popularmovies;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -15,6 +18,8 @@ import java.util.List;
  */
 
 public class MovieAdapter extends ArrayAdapter<Movie> {
+
+    private final String LOG_TAG = MovieAdapter.class.getSimpleName();
 
     public MovieAdapter(Context context, List<Movie> movies) {
         super(context, 0, movies);
@@ -43,7 +48,8 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
         }
 
         ImageView posterImage = (ImageView) gridItemView.findViewById(R.id.poster_thumbnail);
-
+        Picasso.with(getContext()).load(movie.getMoviePosterURL()).into(posterImage);
+        Log.v(LOG_TAG, "Setting image " + movie.getMoviePosterURL());
 
         return gridItemView;
     }
